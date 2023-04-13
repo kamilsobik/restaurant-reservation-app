56969d3fe7b91b055d17e728417ff1b8ac427492
Kamil Sobik <kamil.sobik@gmail.com>
2023-04-13 21:32:18 +0200

    refactoring, nested component

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,17 +1,13 @@
-import NavBar from "@/app/components/NavBar";
+import Header from "@/app/components/Header";
 import Form from "./components/Form";
 
 export default function Reserve() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   );
 }
