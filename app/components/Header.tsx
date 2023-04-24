a71324b426a5d3236d626dd58182343042aeff9c
Kamil Sobik <kamil.sobik@gmail.com>
2023-04-24 17:00:25 +0200

    test webhook

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
       <div className="text-center mt-10">
         <h1 className="text-white text-5xl font-bold mb-2">
-          Find your table for any occasion
+          Find your table for any occasion.
         </h1>
         <SearchBar />
       </div>
